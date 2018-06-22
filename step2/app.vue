0312a4980075d20345d3b323af17666803fabef0
fangchungao <fangchungao@rltx.com>
2018-06-22 16:51:20 +0800

    1.add css

--- step2/app.vue
+++ step2/app.vue
@@ -1,6 +1,21 @@
 <template>
-  <div class="app_in">zuguo</div>
+    <div class="main">
+        <div class="app_in">fang</div>
+        <div class="app_out">tang</div>
+    </div>
 </template>
 <script>
-  export default {};
+export default {};
 </script>
+<!-- tip:  -->
+<!-- scoped  if set the css is only apply to this page . else css is global-->
+<style scoped lang="scss" rel="stylesheet/scss">
+.test {
+	background-color: #efefef;
+}
+.main {
+    .app_out {
+        background-color: orange;
+    }
+}
+</style>
